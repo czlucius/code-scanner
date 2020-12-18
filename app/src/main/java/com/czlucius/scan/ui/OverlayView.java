e071a57b8ef99308b0ae1d11dfde35be53728e8a
czlucius <58442255+czlucius@users.noreply.github.com>
2020-12-18 17:12:31 +0800

    Fix OverlayView so that viewfinder is dependant on screen size, and not hardcoded

--- app/src/main/java/com/czlucius/scan/ui/OverlayView.java
+++ app/src/main/java/com/czlucius/scan/ui/OverlayView.java
@@ -24,35 +24,24 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 
-public class OverlayView2 extends View {
+public class OverlayView extends View {
 
     private final Paint paint = new Paint();
-    private int width = 400;
-    private int height = 400;
-    private int alpha = 40;
-    private final RectArea rectArea = new RectArea(width, height);
+    private int alpha = 85;
+    private final RectArea rectArea = new RectArea();
 
-    public OverlayView2(Context context, @Nullable AttributeSet attrs) {
+    public OverlayView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         paint.setColor(Color.BLACK);
         paint.setAlpha(alpha);
     }
 
-
-    public void setWidth(int width) {
-        this.width = width;
-        rectArea.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-        rectArea.height = height;
-    }
 
     public void setAlpha(int alpha) {
         this.alpha = alpha;
@@ -65,17 +54,28 @@ public class OverlayView2 extends View {
         //Draw 4 rects
         float width = getWidth();
         float height = getHeight();
+
+
+        if (getWidth() < getHeight()) {
+            // Portrait
+            rectArea.width = (getRight() - getLeft()) / 3;
+            rectArea.height = (getBottom() - getTop()) / 5;
+        } else {
+            // Landscape
+            rectArea.width = (getRight() - getLeft()) / 5;
+            rectArea.height = (getBottom() - getTop()) / 3;
+
+        }
+        Log.i("OverlayView", "onDraw: " + rectArea.width + "?" + rectArea.height);
+
+
         // Draw around the rect.
         canvas.drawRect(0, 0, width, height / 2 - rectArea.height, paint);
         canvas.drawRect(0, rectArea.height + (height / 2), width, height, paint);
         canvas.drawRect(0, height / 2 - rectArea.height, width / 2 - rectArea.width, height / 2 + rectArea.height, paint);
         canvas.drawRect(width / 2 + rectArea.width, height / 2 - rectArea.height, width, height / 2 + rectArea.height, paint);
-        canvas.drawLine(width / 2 - rectArea.width,
-                height / 2 - rectArea.height,
-                width / 2 + rectArea.width, height / 2 + rectArea.height, paint
-        );
-    }
 
+    }
 
 
     public static class RectArea {
@@ -86,6 +86,8 @@ public class OverlayView2 extends View {
             this.width = width;
             this.height = height;
         }
+
+        public RectArea() {}
     }
 
 
