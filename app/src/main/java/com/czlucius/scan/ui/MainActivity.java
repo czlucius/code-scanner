33cea3a6892f346df1ee9c9458e0c121e99b54c2
czlucius <58442255+czlucius@users.noreply.github.com>
2021-01-10 19:04:05 +0800

    Redirect Bottom Navigation handling to system
    This commit changes the previous manual switching between fragments to the built-in NavigationUI system, which is able to handle corner cases, thus reducing bugs.
    
    Others:
    This commit also changes handleIntent() to route different intents to different fragments.

--- app/src/main/java/com/czlucius/scan/ui/MainActivity.java
+++ app/src/main/java/com/czlucius/scan/ui/MainActivity.java
@@ -20,7 +20,6 @@ package com.czlucius.scan.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -31,8 +30,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.czlucius.scan.R;
 import com.czlucius.scan.databinding.ActivityMainBinding;
 
-import org.json.JSONArray;
-
 /**
  * NOTE:
  *
@@ -42,8 +39,8 @@ import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final String BOTTOM_NAV = "BOTTOM_NAV";
     private ActivityMainBinding binding;
-    private static final int[] topLevelDestIds = {R.id.scannerFragment, R.id.historyFragment, R.id.createFragment};
 
 
     @Override
@@ -58,32 +55,18 @@ public class MainActivity extends AppCompatActivity {
         }
         NavController navController = navHostFragment.getNavController();
 
-        AppBarConfiguration configuration = new AppBarConfiguration.Builder(topLevelDestIds).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, configuration);
+        NavigationUI.setupWithNavController(binding.bottomNav, navController);
 
-        binding.bottomNav.setOnNavigationItemSelectedListener(item -> {
-
-            int itemId = item.getItemId();
-            if (itemId == R.id.scanner) {
-                return navigate(navController, R.id.scannerFragment);
-            } else if (itemId == R.id.history) {
-                return navigate(navController, R.id.historyFragment);
-            } else if (itemId == R.id.create) {
-                return navigate(navController, R.id.createFragment);
-            }
-
-            return false;
-        });
 
         binding.bottomNav.setOnNavigationItemReselectedListener(item -> {});
-
 
         handleIntent(getIntent(), navController);
     }
 
+
     private void handleIntent(Intent intent, NavController navController)  {
         if (intent == null || intent.getType() == null) return;
-        if (intent.getType().equals("image/*")) {
+        if (intent.getType().startsWith("image")) {
             navigate(navController, R.id.scannerFragment);
         } else {
             navigate(navController, R.id.createFragment);
@@ -91,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private boolean navigate(NavController navController, int id) {
+    private void navigate(NavController navController, int id) {
+
         navController.navigate(id);
-        return true;
     }
 
 
