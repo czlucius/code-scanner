8cfd7de63fe7cdb8e093622c4be3be43589853b4
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-02-27 17:10:26 +0800

    For #38: Use own type integers

--- app/src/main/java/com/czlucius/scan/objects/Type.java
+++ app/src/main/java/com/czlucius/scan/objects/Type.java
@@ -18,6 +18,17 @@
 
 package com.czlucius.scan.objects;
 
+
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_CONTACT;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_EMAIL;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_GEOLOCATION;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_PHONE;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_SMS;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_UNKNOWN_OR_TEXT;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_URL;
+import static com.czlucius.scan.objects.Type.TypeInt.TYPE_WIFI;
+
+import androidx.annotation.IntDef;
 import androidx.room.Entity;
 
 import com.czlucius.scan.App;
@@ -37,7 +48,6 @@ import com.czlucius.scan.objects.actions.SMSAction;
 import com.czlucius.scan.objects.actions.URLAction;
 import com.czlucius.scan.objects.actions.ViewLocationAction;
 import com.czlucius.scan.objects.data.Phone;
-import com.google.mlkit.vision.barcode.Barcode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +58,7 @@ import java.util.Objects;
 public class Type {
     private final List<Action> actions;
     private final String typeName;
-    private final int typeInt;
+    private final @TypeInt int typeInt;
 
     public static Type EMAIL;
     public static Type URL;
@@ -58,6 +68,27 @@ public class Type {
     public static Type SMS;
     public static Type PHONE;
     public static Type GEOLOCATION;
+
+    @IntDef({
+            TYPE_EMAIL,
+            TYPE_URL,
+            TYPE_CONTACT,
+            TYPE_WIFI,
+            TYPE_SMS,
+            TYPE_PHONE,
+            TYPE_GEOLOCATION,
+            TYPE_UNKNOWN_OR_TEXT
+    })
+    public static @interface TypeInt {
+        int TYPE_EMAIL = 1;
+        int TYPE_URL = 2;
+        int TYPE_CONTACT = 3;
+        int TYPE_WIFI = 4;
+        int TYPE_SMS = 5;
+        int TYPE_PHONE = 6;
+        int TYPE_GEOLOCATION = 7;
+        int TYPE_UNKNOWN_OR_TEXT = 0;
+    }
 
     private Type(List<Action> actions, String typeName, int typeInt) {
         actions.add(0, CopyAction.getInstance());
@@ -79,25 +110,25 @@ public class Type {
         return typeInt;
     }
 
-    public static Type getTypeFromCode(int barcodeValueType) {
+    public static Type getTypeFromCode(@TypeInt int barcodeValueType) {
 
         // TODO if add more types here, also add @ Data.java
         switch (barcodeValueType) {
-            case Barcode.TYPE_EMAIL:
+            case TYPE_EMAIL:
                 return Type.EMAIL;
-            case Barcode.TYPE_CONTACT_INFO:
+            case TYPE_CONTACT:
                 return Type.CONTACT;
-            case Barcode.TYPE_URL:
+            case TYPE_URL:
                 return Type.URL;
-            case Barcode.TYPE_WIFI:
+            case TYPE_WIFI:
                 return Type.WIFI;
-            case Barcode.TYPE_SMS:
+            case TYPE_SMS:
                 return Type.SMS;
-            case Barcode.TYPE_PHONE:
+            case TYPE_PHONE:
                 return Type.PHONE;
-            case Barcode.TYPE_GEO:
+            case TYPE_GEOLOCATION:
                 return Type.GEOLOCATION;
-            default:
+            case TypeInt.TYPE_UNKNOWN_OR_TEXT: default:
                 return Type.UNKNOWN_OR_TEXT;
         }
     }
@@ -110,26 +141,26 @@ public class Type {
         // Email
         ArrayList<Action> emailActions = new ArrayList<>();
         emailActions.add(EmailAction.getInstance());
-        EMAIL = new Type(emailActions, App.getStringGlobal(R.string.email, "Email"), Barcode.TYPE_EMAIL);
+        EMAIL = new Type(emailActions, App.getStringGlobal(R.string.email, "Email"), TYPE_EMAIL);
 
 
         // Contacts
         ArrayList<Action> contactActions = new ArrayList<>();
         contactActions.add(AddContactAction.getInstance());
-        CONTACT = new Type(contactActions, App.getStringGlobal(R.string.contact, "Contact"), Barcode.TYPE_CONTACT_INFO);
+        CONTACT = new Type(contactActions, App.getStringGlobal(R.string.contact, "Contact"), TYPE_CONTACT);
 
 
         // URL
         ArrayList<Action> urlActions = new ArrayList<>();
         urlActions.add(URLAction.getInstance());
-        URL = new Type(urlActions, App.getStringGlobal(R.string.url, "URL"), Barcode.TYPE_URL);
+        URL = new Type(urlActions, App.getStringGlobal(R.string.url, "URL"), TYPE_URL);
 
         // WiFi
         ArrayList<Action> wifiActions = new ArrayList<>();
         wifiActions.add(CopySSIDAction.getInstance());
         wifiActions.add(CopyPasskeyAction.getInstance());
         wifiActions.add(AddWiFiAction.getInstance());
-        WIFI = new Type(wifiActions, App.getStringGlobal(R.string.wifi, "Wi-Fi"), Barcode.TYPE_WIFI);
+        WIFI = new Type(wifiActions, App.getStringGlobal(R.string.wifi, "Wi-Fi"), TYPE_WIFI);
 
 
         // SMS
@@ -137,22 +168,22 @@ public class Type {
         smsActions.add(CopySMSRecipientAction.getInstance());
         smsActions.add(CopySMSContentsAction.getInstance());
         smsActions.add(SMSAction.getInstance());
-        SMS = new Type(smsActions, App.getStringGlobal(R.string.sms, "SMS"), Barcode.TYPE_SMS);
+        SMS = new Type(smsActions, App.getStringGlobal(R.string.sms, "SMS"), TYPE_SMS);
 
         // Phone
         ArrayList<Action> phoneActions = new ArrayList<>();
         phoneActions.add(new CopyPhoneAction(data -> (Phone) data));
         phoneActions.add(new CallPhoneAction(data -> (Phone) data));
-        PHONE = new Type(phoneActions, App.getStringGlobal(R.string.phone_number, "Phone number"), Barcode.TYPE_PHONE);
+        PHONE = new Type(phoneActions, App.getStringGlobal(R.string.phone_number, "Phone number"), TYPE_PHONE);
 
         // Geolocation
         ArrayList<Action> locationActions = new ArrayList<>();
         locationActions.add(ViewLocationAction.getInstance());
-        GEOLOCATION = new Type(locationActions, App.getStringGlobal(R.string.geolocation, "Geolocation"), Barcode.TYPE_GEO);
+        GEOLOCATION = new Type(locationActions, App.getStringGlobal(R.string.geolocation, "Geolocation"), TYPE_GEOLOCATION);
 
 
         //unknown, same as text
-        UNKNOWN_OR_TEXT = new Type(new ArrayList<>(), App.getStringGlobal(R.string.text, "Text"), Barcode.TYPE_TEXT);
+        UNKNOWN_OR_TEXT = new Type(new ArrayList<>(), App.getStringGlobal(R.string.text, "Text"), TYPE_UNKNOWN_OR_TEXT);
     }
 
     @Override
